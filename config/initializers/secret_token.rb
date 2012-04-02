dbe606c3d56be573172ec87ec95f6b560f193ecf
Brice Texier <brice.texier@ekylibre.org>
2012-04-02 16:31:28 +0200

    First commit with an empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epistola::Application.config.secret_token = '537c7639b9aa5eb0eace000490080c98b3ce564c97c370f68184ea62d188177f6be918ebb5bd161314e518a0f4162aef8d42d584b5c3573bb6faca2773a83bda'
